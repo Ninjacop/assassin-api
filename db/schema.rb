3b2c2d29324389678c4553f55b21bef08ba1d9a8
Ninjacop <ninjacop1234@gmail.com>
2020-03-03 09:55:06 -0500

    change SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_03_133204) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "kills"
