79fd57dbdc0210dbd39e76e684c0d8d202de7894
Ninjacop <ninjacop1234@gmail.com>
2020-03-06 15:05:24 -0500

    Seeding did not work... again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([ name: 'Star Wars' ,  name: 'Lord of the Rings' ])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Player.create!(name: "example", kills: 0, alive: 1)
